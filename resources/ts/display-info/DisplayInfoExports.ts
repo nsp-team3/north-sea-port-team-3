5c541438666419242fad0232dde3e14edf192972
Zefanja Jobse <zefanjaj@gmail.com>
2021-12-10 12:31:57 +0000

    export comment displayinfo

--- resources/ts/display-info/DisplayInfoExports.ts
+++ resources/ts/display-info/DisplayInfoExports.ts
@@ -1,3 +1,6 @@
+/**
+ * Maakt de dispalyinfo beschikbaar via 1 import
+ */
 import DisplayBerthInfo from "./DisplayBerthInfo";
 import DisplayPortInfo from "./DisplayPortInfo";
 import DisplayVesselInfo from "./DisplayVesselInfo";
